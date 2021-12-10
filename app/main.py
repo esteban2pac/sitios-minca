dc7a68d5b340ce48503a16088f4b675796e76c65
esteban <estebandeleon8080@gmail.com>
2021-12-10 15:00:26 -0500

    error resuelto

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
-from fastapi.middleware import CORSMiddleware
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.api import api_router
+from app.database.session import engine, Base
+from app.core.config  import settings
+ 
 app = FastAPI()
+ 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+   CORSMiddleware,
+   allow_origins=["http://localhost:8080"],
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
 )
-
-@app.get("/health-check/")
+ 
+app.include_router(api_router, prefix=settings.API_V1_STR)
+ 
+@app.get("/health-check")
 def health_check():
-    return "Ok"
+   return "OK"
